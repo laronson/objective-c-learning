b792bc8e8ad71a5b0c3febaaaf6c275dfaf6723b
laronson <laronson21@gmail.com>
2025-03-14 12:03:08 -0700

    Initial Commit

---
+++ objectiveCLearning/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  objectiveCLearning
+//
+//  Created by Lenny on 3/14/25.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
